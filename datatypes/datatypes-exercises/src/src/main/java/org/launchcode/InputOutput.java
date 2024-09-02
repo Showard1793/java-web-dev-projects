f22621bc5913186e5bcbd01ac67358cd266ac8cb
Stephen Howard <Showard1793@gmail.com>
2024-09-02 12:15:22 -0500

    Updated

---
+++ datatypes/datatypes-exercises/src/src/main/java/org/launchcode/InputOutput.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello! What is your name? ");
+
+        String name = input.next();
+        System.out.println("Hello, " + name + "!");
+    }
+}
