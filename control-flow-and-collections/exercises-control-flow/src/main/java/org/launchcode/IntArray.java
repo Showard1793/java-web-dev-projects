ccfb49706acee95600a32131e6e4e56eb716d857
Stephen Howard <Showard1793@gmail.com>
2024-09-07 11:51:03 -0500

    Updated

---
+++ control-flow-and-collections/exercises-control-flow/src/main/java/org/launchcode/IntArray.java
@@ -0,0 +1,13 @@
+package org.launchcode;
+
+public class IntArray {
+    public static void main(String[] args) {
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+
+        for (int i = 0; i < integerArray.length; i++) {
+            if (integerArray[i] % 2 != 0){
+                System.out.println(integerArray[i]);
+            }
+        }
+    }
+}
